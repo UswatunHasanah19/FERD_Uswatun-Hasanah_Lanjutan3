41ae047320e61d3e32440eb67da2c52ec8d78973
Uswatun Hasanah <uh74925@gmail.com>
2021-12-01 14:13:19 +0700

    Tugas Sesi 23

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,12 @@ const Home = ({navigation }) => {
     return( 
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text> Home Page </Text>
+
             <Button 
             title={"ke  halaman Profile"} any
             onPress={() => navigation.navigate("Profile")}
             />
+            
         </View>
     );
 }
